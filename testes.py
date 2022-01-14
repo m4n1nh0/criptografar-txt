593d9e80ad5727dc13c2e97340a808905b268df4
Vitor Vicente <vitor.v@ambevtech.com.br>
2022-01-14 16:38:32 -0300

    testando

--- testes.py
+++ testes.py
@@ -7,9 +7,31 @@ from codificador import Codificar
 
 
 class CodificadorTestes(unittest.TestCase):
-    pass
+    
+    obj = Codificar()
+
+    def test_len_msg_str(self):
+        self.assertEqual(
+            self.obj._len_msg_str('Mensagem'),
+            '000008'
+        )
 
 
-print(f'valor da name: [{__name__}]')
+    def test_len_msg(self):
+        self.assertEqual(
+            self.obj._len_msg('coded=222333kkklll'),
+            111222
+        )
+
+    def test_integridade(self):
+        text = 'Um mensagem qualquer! '
+        obj = Codificar(text)
+        self.assertEqual(
+            self.obj.entrada,
+            text
+        )
+
+
 if __name__ == '__main__':
+    print(f'\n' * 10)
     unittest.main()
---
+++ x.py
@@ -0,0 +1,9 @@
+
+
+from codificador import Codificar
+
+
+text = 'Um mensagem qualquer! '
+obj = Codificar(text)
+
+print(obj)
